7185b3dd6d842bc7a0fba5a8ba750a437cc9c4d3
steekam <waynewanyee@gmail.com>
2021-04-27 20:48:27 +0300

    wip: simple project directory site

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: {
+        content: [
+            './index.html',
+        ]
+    },
+    important: true,
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
